345e89c002232cc5bd31b1a3d638dc6906d35a88
fculibao <ferdieculibao976@gmail.com>
2022-08-24 23:19:46 -0400

    update

--- file/selenium-app01.py
+++ file/selenium-app01.py
@@ -1,8 +1,5 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome(executable_path="./home/ferdie/selenium-project/chromedriver")
 browser.get("http://192.168.1.229:8080/vproapp/login")
 print('Title: %s' % browser.title)
-time.sleep(10)
-browser.quit()
