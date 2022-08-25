fd13ea7d72b7b69af9c6d3e4511bdfe96785d30c
fculibao <ferdieculibao976@gmail.com>
2022-08-24 23:20:49 -0400

    update

--- file/selenium-app01.py
+++ file/selenium-app01.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome(executable_path="./home/ferdie/selenium-project/chromedriver")
 browser.get("http://192.168.1.229:8080/vproapp/login")
 print('Title: %s' % browser.title)
